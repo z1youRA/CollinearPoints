f05b235510ad96968d73319c07625d7c7e6e3a47
z1youra <374671110@qq.com>
2022-03-20 15:51:39 +0800

    remove unit test

--- Point.java
+++ Point.java
@@ -66,11 +66,11 @@ public class Point implements Comparable<Point> {
         else if (that.x == this.x) {
             return Double.POSITIVE_INFINITY;
         }
-        else if (that.y == that.x) {
-            return 0.0;
+        else if (that.y == this.y) {
+            return +0.0;
         }
         else {
-            return ((double) (that.y - this.y)) / (that.x - this.x);
+            return ((double) (this.y - that.y)) / (this.x - that.x);
         }
     }
 
@@ -87,7 +87,12 @@ public class Point implements Comparable<Point> {
      * argument point
      */
     public int compareTo(Point that) {
-        /* YOUR CODE HERE */
+        if (this.y != that.y) {
+            return this.y - that.y;
+        }
+        else {
+            return this.x - that.x;
+        }
     }
 
     /**
@@ -97,9 +102,21 @@ public class Point implements Comparable<Point> {
      * @return the Comparator that defines this ordering on points
      */
     public Comparator<Point> slopeOrder() {
-        /* YOUR CODE HERE */
+        return new BySlope();
     }
 
+    private class BySlope implements Comparator<Point> {
+        public int compare(Point p1, Point p2) {
+            if (slopeTo(p1) < slopeTo(p2)) {
+                return -1;
+            }
+            else if (slopeTo(p1) > slopeTo(p2)) {
+                return +1;
+            }
+            return 0;
+        }
+
+    }
 
     /**
      * Returns a string representation of this point.
