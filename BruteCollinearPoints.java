3de1cdeace2b1047388ba616309866cd4ed50dce
z1youra <374671110@qq.com>
2022-03-21 11:29:04 +0800

    reconstruct code structure, increase security

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -2,56 +2,67 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BruteCollinearPoints {
-    private LineSegment[] lines = new LineSegment[100];
-    private LineSegment[] linesResult;
+    private final LineSegment[] linesResult;
+    private final Point[] points;
     private int linesNum = 0;
 
     public BruteCollinearPoints(Point[] points) {   // finds all line segments containing 4 points
-        Point starts[] = new Point[100];
-        Point ends[] = new Point[100];
-        for (int i = 0; i < points.length; i++) {
-            if (points[i] == null) {
+
+        this.points = Arrays.copyOf(points, points.length);
+        for (int i = 0; i < this.points.length; i++) {
+            if (this.points[i] == null) {
                 throw new IllegalArgumentException("Input Invalid: null points are given");
             }
-            for (int j = i + 1; j < points.length; j++) {
-                if (points[i].slopeTo(points[j]) == Double.NEGATIVE_INFINITY) {
+            for (int j = i + 1; j < this.points.length; j++) {
+                if (this.points[i].slopeTo(this.points[j]) == Double.NEGATIVE_INFINITY) {
                     throw new IllegalArgumentException("Input Invalid: Repeated points are given");
                 }
             }
         }
-        for (int i = 0; i < points.length; i++) {
-            for (int j = i + 1; j < points.length; j++) {
-                double slope1 = points[j].slopeTo(points[i]);   // record the slope from i to j
-                for (int k = j + 1; k < points.length; k++) {
-                    if (points[k].slopeTo(points[j]) == slope1) {    // k is collinear to i and j
-                        for (int m = k + 1; m < points.length; m++) {
-                            if (points[m].slopeTo(points[k]) == slope1) {
+        linesResult = findLines();
+    }
+
+    private LineSegment[] findLines() {
+        ArrayList<LineSegment> lines = new ArrayList<>();
+        Point starts[] = new Point[100];
+        Point ends[] = new Point[100];
+        for (int i = 0; i < this.points.length; i++) {
+            for (int j = i + 1; j < this.points.length; j++) {
+                double slope1 = this.points[j]
+                        .slopeTo(this.points[i]);   // record the slope from i to j
+                for (int k = j + 1; k < this.points.length; k++) {
+                    if (this.points[k].slopeTo(this.points[j])
+                            == slope1) {    // k is collinear to i and j
+                        for (int m = k + 1; m < this.points.length; m++) {
+                            if (this.points[m].slopeTo(this.points[k]) == slope1) {
                                 int little, large, flag = 0;
                                 int ijMin, kmMin;
-                                ijMin = points[i].compareTo(points[j]) < 0 ? i : j;
-                                kmMin = points[k].compareTo(points[m]) < 0 ? k : m;
-                                little = points[ijMin].compareTo(points[kmMin]) < 0 ? ijMin : kmMin;
-                                large = points[i + j - ijMin].compareTo(points[k + m - kmMin]) > 0 ?
+                                // find the start and end of the line.
+                                ijMin = this.points[i].compareTo(this.points[j]) < 0 ? i : j;
+                                kmMin = this.points[k].compareTo(this.points[m]) < 0 ? k : m;
+                                little = this.points[ijMin].compareTo(this.points[kmMin]) < 0 ?
+                                         ijMin : kmMin;
+                                large = this.points[i + j - ijMin]
+                                                .compareTo(this.points[k + m - kmMin]) > 0 ?
                                         i + j - ijMin :
                                         k + m - kmMin;
                                 for (int index = 0; index < linesNum;
                                      index++) {    // check repetition of line little to large;
-                                    if (starts[index] == points[little]
-                                            && ends[index] == points[large]) {
+                                    if (starts[index] == this.points[little]
+                                            && ends[index] == this.points[large]) {
                                         flag = 1;
                                         break;
                                     }
                                 }
                                 if (flag == 0) {
-                                    starts[linesNum] = points[little];
-                                    ends[linesNum] = points[large];
-                                    lines[linesNum++] = new LineSegment(points[little],
-                                                                        points[large]);
-                                }
-                                if (linesNum >= 100) {
-                                    throw new UnsupportedOperationException(
-                                            "Lines number exceeded");
+                                    starts[linesNum] = this.points[little];
+                                    ends[linesNum] = this.points[large];
+                                    lines.add(new LineSegment(this.points[little],
+                                                              this.points[large]));
                                 }
                             }
                         }
@@ -60,10 +71,8 @@ public class BruteCollinearPoints {
                 }
             }
         }
-        linesResult = new LineSegment[linesNum];
-        for (int i = 0; i < linesNum; i++) {
-            linesResult[i] = lines[i];
-        }
+        LineSegment[] segments = new LineSegment[lines.size()];
+        return lines.toArray(segments);
     }
 
     public int numberOfSegments() {     // the number of line segments
@@ -71,7 +80,7 @@ public class BruteCollinearPoints {
     }
 
     public LineSegment[] segments() {     // the line segments
-        return linesResult;
+        return Arrays.copyOf(linesResult, linesResult.length);
     }
 
     public static void main(String[] args) {
